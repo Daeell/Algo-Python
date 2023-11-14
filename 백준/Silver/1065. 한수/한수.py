91c8c44a87647fb523b09369605cb651db947ea4
김다엘 <104256143+Daeell@users.noreply.github.com>
2023-11-15 00:16:57 +0900

    [Silver IV] Title: 한수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.py
+++ 백준/Silver/1065. 한수/한수.py
@@ -2,18 +2,12 @@ N = int(input())
 cnt = 0
 
 for i in range(1, N + 1):
-    n = str(i)
-    if len(n) == 1 or len(n) == 2:
+    sn = str(i)
+    if len(sn) <= 1:
         cnt += 1
     else:
-        itv = int(n[1]) - int(n[0])
-        flag = True
-        for j in range(2, len(n)):
-            tmp = int(n[j]) - int(n[j - 1])
-            if itv != tmp:
-                flag = False
-                break
-        if flag == True:
+        ivt = int(sn[1]) - int(sn[0])
+        if all(int(sn[j]) - int(sn[j - 1]) == ivt for j in range(2, len(sn))):
             cnt += 1
 
 print(cnt)

9b94d29d100bb68cd9737705978b83ab8fc9bd08
김다엘 <104256143+Daeell@users.noreply.github.com>
2023-12-06 01:30:02 +0900

    Time: 392 ms (75.95%), Space: 16.4 MB (31.02%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,6 +1,6 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        def check(l, r):
+        def getMaximumPalindrome(l, r):
             while(l >= 0 and r < len(s) and s[l] == s[r]):
                 l -= 1
                 r += 1
@@ -8,11 +8,8 @@ class Solution:
         
         res = ""
         for i in range(len(s)):
-            odd = check(i, i)
-            even = check(i, i+1)
+            odd = getMaximumPalindrome(i, i)
+            even = getMaximumPalindrome(i, i+1)
             
-            if len(odd) > len(res):
-                res = odd
-            if len(even) > len(res):
-                res = even
+            res = max(odd, even, res, key=len)
         return res

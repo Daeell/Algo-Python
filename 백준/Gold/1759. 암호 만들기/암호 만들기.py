eae6a707ff2bb4d0745dfaf6e8c24c308dc4552a
김다엘 <104256143+Daeell@users.noreply.github.com>
2024-08-26 14:49:22 +0900

    [Gold V] Title: 암호 만들기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -1,31 +1,20 @@
-def comb(index, level):
-	global L, C, input_list, choose, vowel, VC, CC
-	if level == L:
-		if VC <= 0 and CC <=0:
-			for c in choose:
-				print(c, end="")
-			print()
-		return
-	for i in range(index, C):
-		ac = input_list[i]
-		if ac in vowel:
-			VC -= 1
-		else:
-			CC -= 1
-		choose.append(ac)
-		comb(i+1, level+1)
-		pc = choose.pop()
-		if pc in vowel:
-			VC += 1
-		else:
-			CC += 1
+from itertools import combinations
 
-	
+vowels = ['a','e','i','o','u']
+
+def is_possible(word):
+	global L
+	vow = 0
+	for c in word:
+		vow += (c in vowels)
+	con = L - vow
+
+	return (vow >= 1 and con >= 2)
 
 L, C = map(int, input().split())
-input_list = input().split()
-input_list.sort()
-choose = []
-vowel = ["a", "e", "i", "o", "u"]
-VC, CC = 1, 2
-comb(0,0)
+arr = input().split()
+arr.sort()
+
+for word in combinations(arr,L):
+	if is_possible(word):
+		print(''.join(word))
